3e8849048268d376b67574b765773bd6c51d521a
Anita <a.nita@hotmail.co.uk>
2015-02-25 14:26:38 +0000

    Added function range which returns range of numbers up to certain one which you define.

--- chapter4_the_sum_of_range.js
+++ chapter4_the_sum_of_range.js
@@ -58,3 +58,11 @@ function range(start, end, step) {
 }
 
 range(5, 2, -2);
+
+function range(upto) {
+  var result = [];
+  for (var i = 0; i <= upto; i++)
+    result[i] = i;
+  return result;
+}
+range(4);
